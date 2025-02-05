3c189130a98f69880daee5f1d6303a83caee6d6b
Teja_Royal <161200555+teja94411@users.noreply.github.com>
2025-02-06 02:36:36 +0530

    Update main.tf

--- Resources/main.tf
+++ Resources/main.tf
@@ -1,9 +1,18 @@
-module "ec2" {
-  source = "./modules/ec2"
-}
+//module "ec2" {
+ // source = "./modules/ec2"
+//}
 
-module "s3" {
-  source = "./modules/s3" 
+//module "s3" {
+ // source = "./modules/s3" 
+//}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
 }
 
 
